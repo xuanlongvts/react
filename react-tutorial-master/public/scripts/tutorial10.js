4db55d47704bf72343b07272770619d5be9e7556
long.le <long.le@tiki.vn>
2016-07-01 14:51:42 +0700

    [learning react] change struct

---
+++ react-tutorial-master/public/scripts/tutorial10.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tikier on 7/1/16.
+ */
+var CommentList = React.createClass({
+    render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author = {comment.author} key = {comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+
+            return(
+                <div className="commentList">
+                    {commentNodes}
+                </div>
+            );
+        })
+    }
+});
---
+++ react-tutorial-master/public/scripts/tutorial8.js
@@ -0,0 +1,7 @@
+/**
+ * Created by tikier on 7/1/16.
+ */
+var data = [
+    {id: 1, author: "Pete Hunt", text: "This is one comment"},
+    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
