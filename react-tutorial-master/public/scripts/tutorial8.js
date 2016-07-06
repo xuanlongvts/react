e37e3e893465ea3f7032b8c4c08a6206dd128b0f
long.le <long.le@tiki.vn>
2016-07-06 17:39:23 +0700

    [learning react] add js8

--- react-tutorial-master/public/scripts/tutorial8.js
+++ react-tutorial-master/public/scripts/tutorial8.js
@@ -1,14 +1,9 @@
-/*var data = [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];*/
-
 var Comment = React.createClass({
     render: function () {
         return(
             <div className="comment">
-                <h2 className="author">{this.props.author}</h2>
-                <p className="txtComment">{this.props.children}</p>
+                <h2 className="nameAuthor">{this.props.author}</h2>
+                <div className="comment-txt">{this.props.children}</div>
             </div>
         );
     }
@@ -17,14 +12,13 @@ var Comment = React.createClass({
 var CommentList = React.createClass({
     render: function () {
         var commentNodes = this.props.data.map(function (comment) {
-                return (
-                    <Comment author={comment.author} key={comment.id}>
-                        {comment.text}
-                    </Comment>
-                );
-            });
-
-        return (
+            return(
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return(
             <div className="commentList">
                 {commentNodes}
             </div>
@@ -34,7 +28,7 @@ var CommentList = React.createClass({
 
 var CommentForm = React.createClass({
     getInitialState: function () {
-        console.log('Khoi tao khi form duoc goi');
+        console.log('Khoi tao Form khi duoc goi');
         return {
             author: '',
             text: ''
@@ -43,7 +37,7 @@ var CommentForm = React.createClass({
     handleAuthorChange: function (e) {
         this.setState({
             author: e.target.value
-        });
+        })
     },
     handleTextChange: function (e) {
         this.setState({
@@ -67,11 +61,19 @@ var CommentForm = React.createClass({
         });
     },
     render: function () {
-        return (
-            <form className="commentForm" onSubmit={this.handleSubmit} >
-                <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} /><br />
-                <input type="text" placeholder="Say something ..." value={this.state.text}  onChange={this.handleTextChange} />
-                <input type="submit" value="Post" />
+        return(
+            <form className="frmComment" onSubmit={this.handleSubmit}>
+                <label>
+                    <span>Your name: </span>
+                    <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+                </label>
+                <label>
+                    <span>Your comment: </span>
+                    <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+                </label>
+                <p>
+                    <input type="submit" value="Submit" className="btn-submit" />
+                </p>
             </form>
         );
     }
@@ -79,10 +81,8 @@ var CommentForm = React.createClass({
 
 var CommentBox = React.createClass({
     getInitialState: function () {
-        console.log('Khoi tao khi box duoc goi');
-        return {
-            data: []
-        };
+        console.log('Khoi tao CommentBox dau tien khi duoc goi');
+        return {data: []};
     },
     loadCommentsFromServer: function () {
         $.ajax({
@@ -90,26 +90,7 @@ var CommentBox = React.createClass({
             dataType: 'json',
             cache: false,
             success: function (data) {
-                console.log('success');
                 this.setState({data: data});
-            }.bind(this),
-            error: function (xhr, status, err) {
-                console.log(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
-    handleCommentSubmit: function (comment) {
-        console.log('Form submit');
-
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            type: 'POST',
-            data: comment,
-            success: function (data) {
-                this.setState({
-                    data: data
-                });
             }.bind(this),
             error: function (xhr, status, err) {
                 console.log(this.props.url, status, err.toString());
@@ -119,12 +100,31 @@ var CommentBox = React.createClass({
     componentDidMount: function () {
         console.log('componentDidMount');
         this.loadCommentsFromServer();
-        setInterval(this.loadCommentsFromServer, 2000);
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+    handleCommentSubmit: function (comment) {
+        var comments = this.state.data;
+        comment.id = Date.now();
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                this.setState({data: comments});
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        })
     },
     render: function () {
         return(
             <div className="commentBox">
-                <h1>Comments</h1>
+                <h1>List Comment User</h1>
                 <CommentList data={this.state.data} />
                 <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
@@ -133,6 +133,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-    <CommentBox url="/api/comments" />,
+    <CommentBox url="/api/comments" pollInterval={2000} />,
     document.getElementById('content')
 );
