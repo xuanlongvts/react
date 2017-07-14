3b241818d88e91920ba2d8331cf49d1c88fd4ee9
long.le <xuanlongvts@gmail.com>
2017-07-14 14:39:28 +0700

    chat-application

---
+++ chat-app/src/index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+
+    socket.on('disconnect', function () {
+        //console.log('user disconnected');
+    });
+
+    socket.on('chat_mess', function (msg) {
+        //console.log('chat_mess: ', msg);
+        io.emit('chat_mess', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on 3000');
+});
